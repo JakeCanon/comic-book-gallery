56f5896665e3e74de6fcb9059a6cbd1ecf7bf27a
JakeCanon <montecowboy@gmail.com>
2017-06-05 23:32:54 -0400

    Updated Comic Book Detail view to be strongly typed

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -10,11 +10,14 @@ namespace ComicBookGallery.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; internal set; }
+        public Artist[] Artist { get; internal set; }
+
         public DescriptionHtml  { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
-        public atring DisplayText
+        public string DisplayText
         {
             get
             {
